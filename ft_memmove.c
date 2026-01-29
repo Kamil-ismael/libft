47d8d45747053b8a30c87b49da35b2bd77ad862b
Tiana Ismael Kamil Houssen <tkamil-h@c1r12s11.42antananarivo.mg>
2026-01-29 15:31:46 +0300

    add ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,27 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkamil-h <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/27 15:55:38 by tkamil-h          #+#    #+#             */
-/*   Updated: 2026/01/29 15:26:54 by tkamil-h         ###   ########.fr       */
+/*   Created: 2026/01/29 14:12:59 by tkamil-h          #+#    #+#             */
+/*   Updated: 2026/01/29 15:31:15 by tkamil-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*d;
 	unsigned char	*s;
 
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
-	while (n > 0)
+	if (!dest && !src)
+		return (NULL);
+	if (d < s)
 	{
-		*d++ = *s++;
-		n--;
+		while (n--)
+		{
+			*d++ = *s++;
+		}
 	}
-	d[n] = '\0';
+	else
+	{
+		while (n > 0)
+		{
+			d[n - 1] = s[n - 1];
+			n--;
+		}
+	}
 	return (dest);
 }
