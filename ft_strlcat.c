da4fdc11321cb38e3fcd0e5614244b32983f2f09
Tiana Ismael Kamil Houssen <tkamil-h@c1r11s4.42antananarivo.mg>
2026-01-31 14:10:29 +0300

    add ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkamil-h <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/31 12:49:20 by tkamil-h          #+#    #+#             */
+/*   Updated: 2026/01/31 14:09:33 by tkamil-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	dest_len;
+	size_t	i;
+
+	src_len = 0;
+	dest_len = 0;
+	i = 0;
+	while (src[src_len] != '\0')
+		src_len++;
+	while (dest[dest_len] != '\0' && dest_len < size)
+		dest_len++;
+	if (dest_len >= size)
+		return (size + src_len);
+	while (src[i] != '\0' && (dest_len + i) < (size - 1))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
