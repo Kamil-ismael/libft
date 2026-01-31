7ddde1356a240310157f4448ce907f0390076cd4
Tiana Ismael Kamil Houssen <tkamil-h@c1r11s4.42antananarivo.mg>
2026-01-31 18:07:33 +0300

    add ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,27 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkamil-h <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/31 15:22:08 by tkamil-h          #+#    #+#             */
-/*   Updated: 2026/01/31 18:06:58 by tkamil-h         ###   ########.fr       */
+/*   Created: 2026/01/31 17:21:27 by tkamil-h          #+#    #+#             */
+/*   Updated: 2026/01/31 18:03:56 by tkamil-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
+	char			*last;
 	unsigned char	character;
 
 	character = (unsigned char)c;
+	last = NULL;
 	while (*s != '\0')
 	{
 		if (*s == character)
-			return (((char *)s));
+			last = ((char *)s);
 		s++;
 	}
-	if (character == '\0')
-		return ((char *)s);
-	return (NULL);
+	if (*s == character)
+		last = ((char *)s);
+	return (last);
 }
