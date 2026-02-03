08f4742e00d7c05b7b3e7490f368f0a53179031b
Tiana Ismael Kamil Houssen <tkamil-h@c1r11s12.42antananarivo.mg>
2026-02-03 10:40:43 +0300

    modified project

--- ft_strlen.c
+++ ft_strlen.c
@@ -12,12 +12,11 @@
 
 int	ft_strlen(const char *s)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (*s != '\0')
+	while (s[i])
 	{
-		s++;
 		i++;
 	}
 	return (i);
--- libft.h
+++ libft.h
@@ -6,12 +6,30 @@
 /*   By: tkamil-h <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 16:38:49 by tkamil-h          #+#    #+#             */
-/*   Updated: 2026/01/20 16:43:06 by tkamil-h         ###   ########.fr       */
+/*   Updated: 2026/02/03 10:31:41 by tkamil-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isdigit(int c);
+# include <stddef.h>
+
+int		ft_isdigit(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+
 #endif
