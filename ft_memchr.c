2aa910ffaed6c0676a719267b6aa4c531515147d
Tiana Ismael Kamil Houssen <tkamil-h@student.42antananarivo.mg>
2026-02-03 15:11:52 +0300

    add ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,18 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkamil-h <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/20 14:53:57 by tkamil-h          #+#    #+#             */
-/*   Updated: 2026/02/03 15:11:04 by tkamil-h         ###   ########.fr       */
+/*   Created: 2026/02/03 14:07:33 by tkamil-h          #+#    #+#             */
+/*   Updated: 2026/02/03 15:10:03 by tkamil-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-int	ft_isalpha(int c)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	unsigned char	*str;
+	unsigned char	chr;
+	size_t			i;
+
+	str = (unsigned char *)s;
+	chr = (unsigned char )c;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == chr)
+			return (&str[i]);
+		i++;
+	}
+	return (NULL);
 }
