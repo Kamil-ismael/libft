33076133418e47b1a05f6157e71a394e1bdc7c78
Tiana Ismael Kamil Houssen <tkamil-h@student.42antananarivo.mg>
2026-02-04 10:59:03 +0300

    update libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tkamil-h <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 16:38:49 by tkamil-h          #+#    #+#             */
-/*   Updated: 2026/02/03 13:27:48 by tkamil-h         ###   ########.fr       */
+/*   Updated: 2026/02/04 10:53:18 by tkamil-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,13 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *s1, const char *s2, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_atoi(const char *nptr);
 #endif
