8ced54ca7713799b55423e96bdd3f8d7091581d3
Tiana Ismael Kamil Houssen <tkamil-h@student.42antananarivo.mg>
2026-02-05 12:25:18 +0300

    add ft_strtrim

--- Makefile
+++ Makefile
@@ -26,7 +26,8 @@ SRC     = ft_isdigit.c \
           ft_calloc.c \
           ft_strdup.c \
           ft_substr.c \
-          ft_strjoin.c
+          ft_strjoin.c \
+          ft_strtrim.c
 
 OBJ     = $(SRC:.c=.o)
 HEAD    = libft.h
---
+++ ft_strtrim.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkamil-h <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/05 11:31:03 by tkamil-h          #+#    #+#             */
+/*   Updated: 2026/02/05 12:18:49 by tkamil-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	size_t	start;
+	size_t	end;
+	size_t	i;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	if (s1[start] == '\0')
+		return (ft_strdup(""));
+	end = ft_strlen(s1) - 1;
+	while (end > start && ft_strchr(set, s1[end]))
+		end--;
+	str = malloc(sizeof(char) * (end - start + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (start <= end)
+		str[i++] = s1[start++];
+	str[i] = '\0';
+	return (str);
+}
